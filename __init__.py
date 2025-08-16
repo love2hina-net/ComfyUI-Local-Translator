b1570638e1690067551a61668eadc986e57c8198
love2hina <webmaster@love2hina.net>
2025-08-16 16:54:55 +0900

    bump up

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ __all__ = [
 
 __author__ = 'love2hina'
 __email__ = 'webmaster@love2hina.net'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 NODE_CLASS_MAPPINGS = {
     'LocalTranslator': LocalTranslatorNode,
